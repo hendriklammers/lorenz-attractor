4cb55db6098fea1d6c4fa7ba76b5b08ba4940a60
Hendrik Lammers <lammers@gmail.com>
2017-12-03 11:50:04 +0700

    Render a sphere with particles

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,16 @@ import {
   PointLight,
   FogExp2,
   Vector3,
+  Math as Math3,
 } from "three"
 
+const colors = {
+  green: 0xd2fb78,
+  purple: 0xc13bfe,
+  blue: 0x5821d4,
+  cyan: 0x49cdf6,
+  grey: 0x49496a,
+}
 const container = document.querySelector("#container")
 const width = window.innerWidth
 const height = window.innerHeight
@@ -18,10 +26,27 @@ const height = window.innerHeight
 let camera: PerspectiveCamera
 let scene: Scene
 let renderer: WebGLRenderer
-let geometry: Geometry
-let particles: Points
 
-const init = () => {
+const createParticles = () => {
+  const geometry = new Geometry()
+  const material = new PointsMaterial({ size: 3, color: colors.green })
+  const particles = new Points(geometry, material)
+  scene.add(particles)
+
+  const radius = 500
+  for (let i = 0; i < 10000; i++) {
+    const vertex = new Vector3()
+    const theta = Math3.randFloatSpread(360)
+    const phi = Math3.randFloatSpread(360)
+
+    vertex.x = radius * Math.sin(theta) * Math.cos(phi)
+    vertex.y = radius * Math.sin(theta) * Math.sin(phi)
+    vertex.z = radius * Math.cos(theta)
+    geometry.vertices.push(vertex)
+  }
+}
+
+const main = () => {
   camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
   camera.position.z = 1000
 
@@ -41,17 +66,8 @@ const init = () => {
   pointLight.position.z = 130
   scene.add(pointLight)
 
-  geometry = new Geometry()
-
-  const vertex = new Vector3()
-  vertex.x = 0
-  vertex.y = 0
-  vertex.z = 0
-  geometry.vertices.push(vertex)
-
-  const material = new PointsMaterial({ size: 5, color: 0xd2fb78 })
-  particles = new Points(geometry, material)
-  scene.add(particles)
+  createParticles()
+  render()
 }
 
 const render = () => {
@@ -59,5 +75,4 @@ const render = () => {
   requestAnimationFrame(render)
 }
 
-init()
-render()
+main()
