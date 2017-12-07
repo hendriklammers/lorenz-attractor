462d27365dfc3796c2649a86218046260a4fbc65
Hendrik Lammers <lammers@gmail.com>
2017-12-07 15:40:26 +0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import * as THREE from 'three'
 import OrbitControls from 'orbit-controls-es6'
 import { GUI } from 'dat.gui/build/dat.gui.js'
 
-const container = document.querySelector('#container')
-const width = window.innerWidth
-const height = window.innerHeight
 const settings = {
   sigma: 10,
   rho: 28,
@@ -13,37 +10,14 @@ const settings = {
   color: 0x49cdf6,
   scale: 1.5,
 }
-
+const container = document.querySelector('#container')
+const width = window.innerWidth
+const height = window.innerHeight
 let camera
 let scene
 let renderer
 let controls
 let lorenz
-
-function createVertices() {
-  let x = 0.01
-  let y = 0
-  let z = 0
-  const dt = 0.005
-  const vertices = []
-
-  for (let i = 0; i < 10000; i++) {
-    const vertex = new THREE.Vector3()
-
-    const dx = settings.sigma * (y - x) * dt
-    const dy = (x * (settings.rho - z) - y) * dt
-    const dz = (x * y - settings.beta * z) * dt
-    x = x + dx
-    y = y + dy
-    z = z + dz
-
-    vertex.x = x * settings.scale
-    vertex.y = y * settings.scale
-    vertex.z = z * settings.scale
-    vertices.push(vertex)
-  }
-  return vertices
-}
 
 function handleWindowResize() {
   const width = window.innerWidth
@@ -53,7 +27,7 @@ function handleWindowResize() {
   renderer.setSize(width, height)
 }
 
-function main() {
+function initScene() {
   camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000)
   camera.position.z = 100
 
@@ -76,11 +50,9 @@ function main() {
   })
   lorenz = new THREE.Line(geometry, material)
   scene.add(lorenz)
+}
 
-  window.addEventListener('resize', handleWindowResize)
-
-  render()
-
+function initGui() {
   const gui = new GUI()
   gui.add(settings, 'sigma', 1, 20)
   gui.add(settings, 'rho', 10, 50)
@@ -89,7 +61,28 @@ function main() {
   gui.addColor(settings, 'color')
 }
 
+function createVertices() {
+  const dt = 0.005
+  const vertices = []
+  let x = 0.01
+  let y = 0
+  let z = 0
+
+  for (let i = 0; i < 10000; i++) {
+    const vertex = new THREE.Vector3()
+    x += settings.sigma * (y - x) * dt
+    y += (x * (settings.rho - z) - y) * dt
+    z += (x * y - settings.beta * z) * dt
+    vertex.x = x * settings.scale
+    vertex.y = y * settings.scale
+    vertex.z = z * settings.scale
+    vertices.push(vertex)
+  }
+  return vertices
+}
+
 function render() {
+  // TODO: Only call createVertices when settings are updated
   lorenz.geometry.vertices = createVertices()
   lorenz.geometry.verticesNeedUpdate = true
   lorenz.material.color.setHex(settings.color)
@@ -97,6 +90,13 @@ function render() {
   renderer.render(scene, camera)
   controls.update()
   requestAnimationFrame(render)
+}
+
+function main() {
+  initScene()
+  initGui()
+  window.addEventListener('resize', handleWindowResize)
+  render()
 }
 
 main()
