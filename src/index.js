a1d0b9befd6c04e4319cd65fbe22d250b9286354
Hendrik Lammers <lammers@gmail.com>
2017-12-07 14:24:00 +0700

    Upgrade dat.gui version

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 import './styles.css'
 import * as THREE from 'three'
 import OrbitControls from 'orbit-controls-es6'
+import { GUI } from 'dat.gui/build/dat.gui.js'
 
-const colors = {
-  green: 0xd2fb78,
-  purple: 0xc13bfe,
-  blue: 0x5821d4,
-  cyan: 0x49cdf6,
-  grey: 0x49496a,
-}
+// const colors = {
+//   green: 0xd2fb78,
+//   purple: 0xc13bfe,
+//   blue: 0x5821d4,
+//   cyan: 0x49cdf6,
+//   grey: 0x49496a,
+// }
 const container = document.querySelector('#container')
 const width = window.innerWidth
 const height = window.innerHeight
 const settings = {
-  color: colors.cyan,
+  sigma: 10,
+  rho: 28,
+  beta: 8 / 3,
+  color: 0x49cdf6,
+  scale: 1.5,
 }
 
 let camera
 let scene
 let renderer
 let controls
+let material
 
-const createParticles = () => {
+const createPoints = () => {
   const geometry = new THREE.Geometry()
-  const material = new THREE.LineBasicMaterial({
+  material = new THREE.LineBasicMaterial({
     color: settings.color,
     linewidth: 3,
   })
@@ -32,25 +38,21 @@ const createParticles = () => {
   let x = 0.01
   let y = 0
   let z = 0
-  let a = 10
-  let b = 28
-  let c = 2.5
   const dt = 0.005
-  const scale = 1.5
 
   for (let i = 0; i < 10000; i++) {
     const vertex = new THREE.Vector3()
 
-    const dx = a * (y - x) * dt
-    const dy = (x * (b - z) - y) * dt
-    const dz = (x * y - c * z) * dt
+    const dx = settings.sigma * (y - x) * dt
+    const dy = (x * (settings.rho - z) - y) * dt
+    const dz = (x * y - settings.beta * z) * dt
     x = x + dx
     y = y + dy
     z = z + dz
 
-    vertex.x = x * scale
-    vertex.y = y * scale
-    vertex.z = z * scale
+    vertex.x = x * settings.scale
+    vertex.y = y * settings.scale
+    vertex.z = z * settings.scale
     geometry.vertices.push(vertex)
   }
 
@@ -67,9 +69,7 @@ const handleWindowResize = () => {
 
 const main = () => {
   camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000)
-  controls = new OrbitControls(camera)
   camera.position.z = 100
-  controls.update()
 
   scene = new THREE.Scene()
   scene.fog = new THREE.FogExp2(0x000000, 0.0007)
@@ -80,15 +80,26 @@ const main = () => {
   renderer.setPixelRatio(window.devicePixelRatio)
   container.appendChild(renderer.domElement)
 
-  const particles = createParticles()
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.update()
+
+  const particles = createPoints()
   scene.add(particles)
 
   window.addEventListener('resize', handleWindowResize)
 
   render()
+
+  const gui = new GUI()
+  gui.add(settings, 'sigma')
+  gui.add(settings, 'rho')
+  gui.add(settings, 'beta')
+  gui.add(settings, 'scale', 1, 2)
+  gui.addColor(settings, 'color')
 }
 
 const render = () => {
+  material.color.setHex(settings.color)
   renderer.render(scene, camera)
   controls.update()
   requestAnimationFrame(render)
